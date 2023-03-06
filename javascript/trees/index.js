2554ea1dd0f008377eb0ce7d78a3ed42758df672
Ash Uppal <ashuppal@gmail.com>
2023-03-06 14:00:27 -0800

    completed code challenge 16

--- javascript/trees/index.js
+++ javascript/trees/index.js
@@ -111,6 +111,60 @@ class BinarySearchTree extends Tree {
     return contains;
 
   }
+
+  findMaximumValue(){
+
+    if (this.root===null) throw new Error ('you do not  have a tree');
+    let number=0;
+
+    const traverse=(node)=>{
+
+      if(number<node.value){
+        number=node.value;
+      }
+      if(node.left){
+        traverse(node.left);
+
+      }
+      if(node.right) {
+        traverse(node.right);
+      }
+    };
+    traverse(this.root);
+    return number;
+  }
 }
 
+const tree = new Tree();
+tree.root = new Node(1);
+tree.root.left = new Node(2);
+tree.root.right = new Node(3);
+tree.root.left.left = new Node(4);
+tree.root.left.right = new Node(5);
+tree.root.right.left = new Node(6);
+tree.root.right.right = new Node(7);
+
+const binarySearchTree = new BinarySearchTree();
+binarySearchTree.add(5);
+binarySearchTree.add(3);
+binarySearchTree.add(7);
+binarySearchTree.add(2);
+
+// console.log('preOrder', tree.preOrder());
+// console.log('inOrder', tree.inOrder());
+
+
+// console.log('postOrder', tree.postOrder());
+
+// console.log('contains', binarySearchTree.contains(3));
+// console.log('contains', binarySearchTree.contains(8));
+
+console.log(binarySearchTree.findMaximumValue());
+
+
 module.exports = { Tree, Node, BinarySearchTree};
+
+
+
+
+
